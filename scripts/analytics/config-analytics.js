bab9d884ba8e821d028d19dbf50f3222f96ddd3d
project-orion-site <154313104+project-orion-site@users.noreply.github.com>
2024-03-07 12:32:04 +0200

    Update datastream ID to 76029273-1305-4362-82f3-65749e68521d

--- scripts/analytics/config-analytics.js
+++ scripts/analytics/config-analytics.js
@@ -12,8 +12,9 @@
 
 const configs = {
   tenantId: '_dcbl',
-  edgeConfigId: '4bdc9914-9463-4627-b7ba-048493ab998d',
+  edgeConfigId: '76029273-1305-4362-82f3-65749e68521d',
   orgId: '6D9FE18C5536A5E90A4C98A6@AdobeOrg',
 };
 
 export default configs;
+
